2f0aa3b284780a1e1826d10148560b086952d4e6
Mick Koch <mick@kochm.co>
2015-05-06 07:21:02 -0400

    Boost baud rate

--- src/consts.rs
+++ src/consts.rs
@@ -3,6 +3,6 @@ use std;
 // Samples per second.
 pub const SAMPLES_PER_SEC: u32 = 44000;
 // Symbols per second.
-pub const BAUD: u32 = 60;
+pub const BAUD: u32 = 120;
 // Amplitude for the signal.
 pub const AMPLITUDE: f64 = std::i16::MAX as f64;
