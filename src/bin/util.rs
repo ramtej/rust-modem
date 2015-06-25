401b9ab0fc04a5692596850278e0b63004e38ddc
Mick Koch <mick@kochm.co>
2015-06-25 15:22:48 -0400

    Add Read16 trait

--- src/bin/util.rs
+++ src/bin/util.rs
@@ -5,10 +5,27 @@ pub trait Write16 {
     fn write_i16(&mut self, n: i16) -> std::io::Result<usize>;
 }
 
+pub trait Read16 {
+    fn read_i16(&mut self) -> std::io::Result<i16>;
+}
+
 impl<W> Write16 for W where W: std::io::Write {
     fn write_i16(&mut self, n: i16) -> std::io::Result<usize> {
         let buf: [u8; 2] = unsafe { std::mem::transmute(n) };
         self.write(&buf)
+    }
+}
+
+impl<R> Read16 for R where R: std::io::Read {
+    fn read_i16(&mut self) -> std::io::Result<i16> {
+        let mut buf = [0, 0];
+
+        match self.read(&mut buf) {
+            Ok(2) => Ok(unsafe { std::mem::transmute(buf) }),
+            Ok(_) => Err(std::io::Error::new(std::io::ErrorKind::Other,
+                                             "no more words available")),
+            Err(e) => Err(e),
+        }
     }
 }
 
