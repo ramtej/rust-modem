0c17cf8aabe828729a0ee013bed73b58e525a95e
Mick Koch <mick@kochm.co>
2016-08-21 17:55:42 -0400

    Split out includes

--- src/modem/modulator.rs
+++ src/modem/modulator.rs
@@ -1,6 +1,9 @@
 use num::complex::Complex32;
 
-use super::{phasor, digital, carrier, data};
+use data::{Source, SourceUpdate};
+use phasor::Phasor;
+use carrier::Carrier;
+use digital::DigitalPhasor;
 
 fn real(i: f32, q: f32, cos: f32, sin: f32) -> f32 {
     i * cos - q * sin
@@ -11,19 +14,19 @@ fn imag(i: f32, q: f32, cos: f32, sin: f32) -> f32 {
 }
 
 pub struct Modulator {
-    carrier: carrier::Carrier,
-    phasor: Box<phasor::Phasor>,
+    carrier: Carrier,
+    phasor: Box<Phasor>,
 }
 
 impl Modulator {
-    pub fn new(c: carrier::Carrier, psr: Box<phasor::Phasor>) -> Modulator {
+    pub fn new(c: Carrier, psr: Box<Phasor>) -> Modulator {
         Modulator {
             carrier: c,
             phasor: psr,
         }
     }
 
-    pub fn into_carrier(self) -> carrier::Carrier { self.carrier }
+    pub fn into_carrier(self) -> Carrier { self.carrier }
 }
 
 impl Iterator for Modulator {
@@ -46,16 +49,13 @@ impl Iterator for Modulator {
 
 pub struct DigitalModulator {
     start_sample: usize,
-    data: Box<data::Source>,
-    carrier: carrier::Carrier,
-    phasor: Box<digital::DigitalPhasor>,
+    data: Box<Source>,
+    carrier: Carrier,
+    phasor: Box<DigitalPhasor>,
 }
 
 impl DigitalModulator {
-    pub fn new(c: carrier::Carrier, psr: Box<digital::DigitalPhasor>,
-               src: Box<data::Source>)
-        -> DigitalModulator
-    {
+    pub fn new(c: Carrier, psr: Box<DigitalPhasor>, src: Box<Source>) -> DigitalModulator {
         DigitalModulator {
             start_sample: c.sample + 1,
             data: src,
@@ -64,7 +64,7 @@ impl DigitalModulator {
         }
     }
 
-    pub fn into_carrier(self) -> carrier::Carrier { self.carrier }
+    pub fn into_carrier(self) -> Carrier { self.carrier }
 }
 
 impl Iterator for DigitalModulator {
@@ -74,12 +74,12 @@ impl Iterator for DigitalModulator {
         let phase = self.carrier.next();
 
         let bits = match self.data.update(self.carrier.sample - self.start_sample) {
-            data::SourceUpdate::Finished => return None,
-            data::SourceUpdate::Changed(b) => {
+            SourceUpdate::Finished => return None,
+            SourceUpdate::Changed(b) => {
                 self.phasor.update(self.carrier.sample, b);
                 b
             }
-            data::SourceUpdate::Unchanged(b) => b,
+            SourceUpdate::Unchanged(b) => b,
         };
 
         let (i, q) = self.phasor.next(self.carrier.sample, bits);
