76711c2b5cc217cfd05b3a54c616cabbbedce4c6
Mick Koch <mick@kochm.co>
2016-08-24 18:57:01 -0400

    Sort mods

--- src/modem/digital/mod.rs
+++ src/modem/digital/mod.rs
@@ -2,15 +2,15 @@ mod phasor;
 mod util;
 
 pub mod bask;
-pub mod bpsk;
 pub mod bfsk;
-pub mod oqpsk;
-pub mod qpsk;
-pub mod qam;
-pub mod msk;
-pub mod mfsk;
+pub mod bpsk;
 pub mod cpfsk;
-pub mod mpsk;
 pub mod dqpsk;
+pub mod mfsk;
+pub mod mpsk;
+pub mod msk;
+pub mod oqpsk;
+pub mod qam;
+pub mod qpsk;
 
 pub use self::phasor::DigitalPhasor;
