03dae6e1ce5e81b0847929599e756e224441ce24
Mick Koch <mick@kochm.co>
2016-08-27 16:17:34 -0400

    Don't allow bit periods with fractional sample length in MSK

--- src/modem/digital/msk.rs
+++ src/modem/digital/msk.rs
@@ -5,19 +5,21 @@ use super::util::bit_to_sign;
 
 pub struct MSK {
     amplitude: f32,
-    samples_per_bit: f32,
+    samples_per_bit: usize,
 }
 
 impl MSK {
     pub fn new(amplitude: f32, samples_per_symbol: usize) -> MSK {
+        assert!(samples_per_symbol % 2 == 0);
+
         MSK {
             amplitude: amplitude,
-            samples_per_bit: samples_per_symbol as f32 / 2.0,
+            samples_per_bit: samples_per_symbol / 2,
         }
     }
 
     fn inner(&self, s: usize) -> f32 {
-        PI / 2.0 * s as f32 / self.samples_per_bit
+        PI / 2.0 * s as f32 / self.samples_per_bit as f32
     }
 }
 
